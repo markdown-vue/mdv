735296d0e7c406ebb439c4913802ffc1dbb12947
naminabilzadeh <naminabilzadeh@yahoo.com>
2025-09-12 19:58:13 +0330

    chore: bump version to 1.0.10 and simplify asset copying script

--- scripts/copy-assets.cjs
+++ scripts/copy-assets.cjs
@@ -7,28 +7,10 @@ const path = require("path");
 
 // Components
 const componentsSrc = path.join(__dirname, "../src/components");
-const componentsDistCjs = path.join(__dirname, "../dist/cjs/components");
-const componentsDistEsm = path.join(__dirname, "../dist/esm/components");
+const componentsDist = path.join(__dirname, "../dist/components");
 
 
-fs.rmSync(componentsDistCjs, { recursive: true, force: true });
-fs.rmSync(componentsDistEsm, { recursive: true, force: true });
-
-fs.cpSync(componentsSrc, componentsDistCjs, { recursive: true });
-fs.cpSync(componentsSrc, componentsDistEsm, { recursive: true });
-
-
-// Types index.d.ts
-const typesSrc = path.join(__dirname, "../src/index.d.ts");
-const typesDistCjs = path.join(__dirname, "../dist/cjs/index.d.ts");
-const typesDistEsm = path.join(__dirname, "../dist/esm/index.d.ts");
-
-// Correcting index.d.ts imports
-const indexDts = fs.readFileSync(typesSrc, "utf-8");
-const indexDtsFixed = indexDts.replace(/import (.*?) from '.\/(.*)'/g, "import $1 from '../../src/$2';");
-
-
-fs.writeFileSync(typesDistCjs, indexDtsFixed);
-fs.writeFileSync(typesDistEsm, indexDtsFixed);
+fs.rmSync(componentsDist, { recursive: true, force: true });
+fs.cpSync(componentsSrc, componentsDist, { recursive: true });
 
 console.log(`✅ Copied assets to ./dist/cjs/ and ./dist/esm/`);
